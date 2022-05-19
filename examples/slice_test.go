a9dd2e7b7fb8807816edc70330f25325449b7824
wangyunlong01 <wangyunlong01@inspur.com>
2022-05-19 11:18:20 +0800

    feat: slice test

--- examples/slice_test.go
+++ examples/slice_test.go
@@ -15,3 +15,102 @@ func TestSlice(t *testing.T) {
 	fmt.Println(len(s2))
 	fmt.Println(cap(s2))
 }
+
+type School struct {
+	room []int
+}
+
+func (s School) SetRoom1(rooms []int) {
+	fmt.Printf("%p\n", &s)
+	rooms = append(rooms, 3)
+	s.room = rooms
+}
+
+func (s School) GetRoom1() []int {
+	if s.room != nil {
+		return s.room
+	}
+	return nil
+}
+
+func (s *School) SetRoom2(rooms []int) {
+	fmt.Printf("%p\n", s)
+	rooms = append(rooms, 3)
+	s.room = rooms
+}
+
+func (s *School) GetRoom2() []int {
+	if s.room != nil {
+		return s.room
+	}
+	return nil
+}
+
+func TestInputSlice1(t *testing.T) {
+	s := School{}
+	rooms := []int{1, 2}
+	fmt.Printf("%p\n", &s)
+	s.SetRoom1(rooms)
+	fmt.Println(rooms)
+	fmt.Println(s.GetRoom1())
+}
+
+func TestInputSlice1_1(t *testing.T) {
+	s := &School{}
+	rooms := []int{1, 2}
+	fmt.Printf("%p\n", s)
+	s.SetRoom1(rooms)
+	fmt.Println(rooms)
+	fmt.Println(s.GetRoom1())
+}
+
+func TestInputSlice2(t *testing.T) {
+	s := School{}
+	rooms := []int{1, 2}
+	fmt.Printf("%p\n", &s)
+	s.SetRoom2(rooms)
+	rooms = append(rooms, 4)
+	fmt.Println(rooms)
+	fmt.Println(s.GetRoom2())
+
+	fmt.Printf("%p\n", &s)
+	s.SetRoom2(rooms)
+
+	fmt.Println(rooms)
+	fmt.Println(s.GetRoom2())
+}
+
+func TestInputSlice2_1(t *testing.T) {
+	s := &School{}
+	rooms := []int{1, 2}
+	fmt.Printf("%p\n", s)
+	s.SetRoom2(rooms)
+	fmt.Println(rooms)
+	fmt.Println(s.GetRoom2())
+}
+
+func TestReturnSlice1(t *testing.T) {
+	s := School{}
+	s.room = append(s.room, 1)
+	fmt.Println(s.GetRoom1())
+	rooms := s.GetRoom1()
+	rooms = append(rooms, 2)
+	fmt.Println(rooms)
+	fmt.Println(s.GetRoom1())
+}
+
+func TestReturnSlice2(t *testing.T) {
+	s := School{}
+	s.room = make([]int, 0, 10)
+	s.room = append(s.room, 1)
+	fmt.Println(s.GetRoom2())
+	rooms := s.GetRoom2()
+	rooms = append(rooms, 2)
+	fmt.Printf("%p\n", s.room)
+	fmt.Printf("%p\n", rooms)
+	fmt.Println(rooms)
+	fmt.Println(s.GetRoom2())
+
+	fmt.Println(cap(s.GetRoom2()))
+	fmt.Println(cap(rooms))
+}
