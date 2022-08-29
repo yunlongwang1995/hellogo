477b45ea8eb8bcfb35e55f84bd7bf0c79cf74253
wangyunlong01 <wangyunlong01@inspur.com>
2022-08-29 09:01:35 +0800

    feat: 进制测试

--- main.go
+++ main.go
@@ -11,8 +11,14 @@ import "C"
 import "fmt"
 
 func main() {
+	// CGO 测试
 	v := 42
 	C.printint(C.int(v))
-
 	fmt.Println("hello cgo example")
+
+	// 8进制与16进制
+	const hex = '\x11'
+	fmt.Println(hex)
+	const octal = '\011'
+	fmt.Println(octal)
 }
