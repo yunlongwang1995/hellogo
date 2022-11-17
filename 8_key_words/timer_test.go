d733b0ce30def6bd62a3491621956ddbe2480e7f
wangyunlong01 <wangyunlong01@inspur.com>
2022-11-17 16:48:24 +0800

    feat: timer 测试

--- 8_key_words/timer_test.go
+++ 8_key_words/timer_test.go
@@ -1,7 +1,28 @@
 package examples
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
+import "time"
 
 func Test_timer(t *testing.T) {
+	// now
+	fmt.Println(time.Now().String())
+	timer := time.NewTimer(5 * time.Second)
+	<-timer.C
 
+	// now + 5
+
+	time.AfterFunc(2*time.Second, func() {
+		fmt.Println(time.Now().String())
+	})
+	//fmt.Println("stop timer: ", timer2.Stop())
+
+	timeCh := time.After(5 * time.Second)
+	<-timeCh
+
+	// now + 10
+
+	fmt.Println(time.Now().String())
 }
